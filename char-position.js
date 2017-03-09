0ebb1246954a86bec1c8936dd29ce178d9bdd6a6
shinmike <shinmike90@gmail.com>
2017-03-08 16:39:23 -0800

    Update char-position.js

--- char-position.js
+++ char-position.js
@@ -5,13 +5,31 @@ function countLetters(inputString) {
       // console.log('this is input: ', inputString);
       var characters = inputString[i];
         if (characters in outputObject) {
-          outputObject[characters].push(i);
+          outputObject[characters] = outputObject[characters] + 1;
         } else if (characters != ' ') {
-          outputObject[characters] = [i];
+          outputObject[characters] = 1;
         }
     }
     return outputObject;
 }
 
 console.log(countLetters("lighthouse labs"));
+
+
+
+// write function 'countLetters' that returns all unique characters
+// how many instances of letter found in string
+// return:
+// {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
 
