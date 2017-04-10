c0369d445235e07333a7a8d97dae6ee1f09b6d5d
shinmike <shinmike90@gmail.com>
2017-04-09 21:31:40 -0700

    revised solution with comments

--- char-position.js
+++ char-position.js
@@ -1,35 +1,59 @@
-function countLetters(inputString) {
-  var outputObject = {};
-    for (var i = 0; i < inputString.length; i++) {
-      // console.log('this is i: ', i);
-      // console.log('this is input: ', inputString);
-      var characters = inputString[i];
-        if (characters in outputObject) {
-          outputObject[characters] = outputObject[characters] + 1;
-        } else if (characters != ' ') {
-          outputObject[characters] = 1;
-        }
+function countLetters(inputString){
+  var outputCharacters = {};
+  for (var i = 0; i < inputString.length; i++){
+    // character is key
+    var character = inputString[i].toLowerCase();
+    // if character (key) is in outputCharacters (object)
+    if (character in outputCharacters) {
+      // value of the key = value of the key + 1
+      outputCharacters[character] = outputCharacters[character] + 1;
+      // if the chracter is not a space (' ')
+    } else if (character != ' ') {
+      // then assign value of 1
+      outputCharacters[character] = 1;
     }
-    return outputObject;
+  }
+  return outputCharacters;
 }
 
-console.log(countLetters("lighthouse labs"));
+console.log(countLetters("lighthouse in the house"));
 
 
 
-// write function 'countLetters' that returns all unique characters
-// how many instances of letter found in string
-// return:
-// {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
+
+
+
+// var obj = {a: 1, b: 2, c: 3};
+
+// for (var prop in obj) {
+//   console.log('obj.' + prop, '=', obj[prop]);
 // }
 
+// // prop = a,b,c -> key
+// // obj[prop] = 1,2,3 -> value
+
+// // Output:
+// // "obj.a = 1"
+// // "obj.b = 2"
+// // "obj.c = 3"
+
+
+
+
+
+// ---------------------------------- ORIGINAL SOLUTION:
+
+// function countLetters(inputString) {
+//   var outputObject = {};
+//     for (var i = 0; i < inputString.length; i++) {
+//       var characters = inputString[i];
+//         if (characters in outputObject) {
+//           outputObject[characters] = outputObject[characters] + 1;
+//         } else if (characters != ' ') {
+//           outputObject[characters] = 1;
+//         }
+//     }
+//     return outputObject;
+// }
+
+// console.log(countLetters("lighthouse labs"));
