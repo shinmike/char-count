c3a292deda168f9306d30150b5333301e48ab596
shinmike <shinmike90@gmail.com>
2017-05-31 13:03:36 -0700

    Update char-count.js

--- char-count.js
+++ char-count.js
@@ -1,19 +1,15 @@
 function countLetters(inputString){
-  var outputCharacters = {};
+  var outputObject = {};
+  inputString = inputString.split(" ").join("");
   for (var i = 0; i < inputString.length; i++){
-    // character is key
-    var character = inputString[i].toLowerCase();
-    // if character (key) is in outputCharacters (object)
-    if (character in outputCharacters) {
-      // value of the key = value of the key + 1
-      outputCharacters[character] = outputCharacters[character] + 1;
-      // if the chracter is not a space (' ')
-    } else if (character != ' ') {
-      // then assign value of 1
-      outputCharacters[character] = 1;
+    var character = inputString[i];
+    if (character in outputObject){
+      outputObject[character] += 1;
+    } else {
+      outputObject[character] = 1;
     }
   }
-  return outputCharacters;
+  return outputObject;
 }
 
 console.log(countLetters("lighthouse in the house"));
@@ -22,6 +18,12 @@ console.log(countLetters("lighthouse in the house"));
 
 
 
+
+
+
+
+
+// ---------------------------------- TESTING:
 
 // var obj = {a: 1, b: 2, c: 3};
 
@@ -39,9 +41,7 @@ console.log(countLetters("lighthouse in the house"));
 
 
 
-
-
-// ---------------------------------- ORIGINAL SOLUTION:
+// ---------------------------------- ORIGINAL SOLUTION 1:
 
 // function countLetters(inputString) {
 //   var outputObject = {};
@@ -55,5 +55,28 @@ console.log(countLetters("lighthouse in the house"));
 //     }
 //     return outputObject;
 // }
-
+//
 // console.log(countLetters("lighthouse labs"));
+
+// ---------------------------------- ORIGINAL SOLUTION 2:
+
+// function countLetters(inputString){
+//   var outputCharacters = {};
+//   for (var i = 0; i < inputString.length; i++){
+//     // character is key
+//     var character = inputString[i].toLowerCase();
+//     // if character (key) is in outputCharacters (object)
+//     if (character in outputCharacters) {
+//       // value of the key = value of the key + 1
+//       outputCharacters[character] = outputCharacters[character] + 1;
+//       // if the chracter is not a space (' ')
+//     } else if (character != ' ') {
+//       // then assign value of 1
+//       outputCharacters[character] = 1;
+//     }
+//   }
+//   return outputCharacters;
+// }
+//
+// console.log(countLetters("lighthouse in the house"));
+
